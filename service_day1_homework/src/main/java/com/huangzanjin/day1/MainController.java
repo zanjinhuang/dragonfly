f67edf93cda1bfbbd37c10beb7714ae4c282c92c
huangzj70 <huangzj70@chinaunicom.cn>
2018-07-10 16:09:56 +0800

    提交新架构版本

---
+++ service_day1_homework/src/main/java/com/huangzanjin/day1/MainController.java
@@ -0,0 +1,15 @@
+package com.huangzanjin.day1;
+
+/**
+ * @ClassName:
+ * @Despriction:
+ * @Author: huangzj70@chinaunicom.cn
+ * @Date: Created By 2018/7/10 15:59
+ */
+public class MainController {
+
+	public static void main(String[] args) {
+		System.out.println("=============张三==============");
+	}
+
+}
